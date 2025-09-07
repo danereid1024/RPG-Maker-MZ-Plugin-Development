02574f34df7da8f92216648fd1ef49d1aa02c7d3
danereid <danereidcode@gmail.com>
2025-09-07 11:49:33 -0400

    making altbattlesystem

---
+++ AltBattleSystem.js
@@ -0,0 +1,17 @@
+//=============================================================================
+// RPG Maker MZ - Alternative Battle System
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @plugindesc Alternative battle system. Similar to Final Fantasy Mystic Quest
+ * @author Dane Reid
+ *
+ * @help AltBattleSystem.js
+ *
+ * This plugin changes the layout of sprites during battle.
+ * It places the battlers and enemy battlers vertically to each other.
+ *
+ * It does not provide plugin commands.
+ */
+(() => {})();
